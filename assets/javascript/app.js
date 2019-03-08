08e33b7a0519d47249deefdec2cc92a87e365149
graygabrielle <gray.atkinson.adams@gmail.com>
2019-03-08 13:30:06 -0800

    initialize files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: firebaseAPI,
+    authDomain: "curate-ss.firebaseapp.com",
+    databaseURL: "https://curate-ss.firebaseio.com",
+    projectId: "curate-ss",
+    storageBucket: "curate-ss.appspot.com",
+    messagingSenderId: "872408743875"
+  };
+  firebase.initializeApp(config);
