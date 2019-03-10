0e3983eca458ef4ca6e130f0adf63ebe5cfe2ef7
graygabrielle <gray.atkinson.adams@gmail.com>
2019-03-10 11:49:56 -0700

    initializing files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
   // Initialize Firebase
-  var config = {
+  const config = {
     apiKey: firebaseAPI,
     authDomain: "curate-ss.firebaseapp.com",
     databaseURL: "https://curate-ss.firebaseio.com",
