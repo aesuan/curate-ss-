abb7ef02ff416725310ff9dc54de72b7c74499ac
thirty potatoes <drinkereliza@gmail.com>
2019-03-10 14:03:38 -0700

    initialized js and added paintings array with proto api call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,54 @@
-  // Initialize Firebase
-  const config = {
-    apiKey: firebaseAPI,
-    authDomain: "curate-ss.firebaseapp.com",
-    databaseURL: "https://curate-ss.firebaseio.com",
-    projectId: "curate-ss",
-    storageBucket: "curate-ss.appspot.com",
-    messagingSenderId: "872408743875"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+const config = {
+  apiKey: firebaseAPI,
+  authDomain: "curate-ss.firebaseapp.com",
+  databaseURL: "https://curate-ss.firebaseio.com",
+  projectId: "curate-ss",
+  storageBucket: "curate-ss.appspot.com",
+  messagingSenderId: "872408743875"
+};
+firebase.initializeApp(config);
+
+let paintings = [
+  {
+    paintingName: "Cow's Skull: Red, White, and Blue",
+    objectID: 0,
+    palette: { primaryColor: "", secondaryColor: "", tertiaryColor: "" }
+      //rainyPalette: {}, nightPalette: {} etc 
+    },
+  {
+    paintingName: "The Hunters in the Snow",
+    objectID: 0,
+    palette: { primaryColor: "", secondaryColor: "", tertiaryColor: "" }
+  },
+  {
+    paintingName: "",
+    objectID: 0,
+    palette: { primaryColor: "", secondaryColor: "", tertiaryColor: "" }
+  },
+  {
+    paintingName: "",
+    objectID: 0,
+    palette: { primaryColor: "", secondaryColor: "", tertiaryColor: "" }
+  },
+  {
+    paintingName: "",
+    objectID: 0,
+    palette: { primaryColor: "", secondaryColor: "", tertiaryColor: "" }
+  }
+]
+
+
+//wikipedia API query URL to get first two sentences of article
+//"https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=" + pageName + "&exintro=&exsentences=2&explaintext=&redirects=&format=json"
+
+
+let metQueryURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + paintings[paintingNumber].paintingName;
+
+//Met API ajax call
+$.ajax({
+  url: metQueryURL,
+  method: "GET"
+}) .then(function(response) {
+  
+})
