b4fbd511265bb1894ad0410f17cbf1fb7f15f69f
graygabrielle <gray.atkinson.adams@gmail.com>
2019-03-11 19:52:20 -0700

    fixed orientation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,7 @@ let paintings = [
     paintingName: "The Harvesters",
     objectID: 435809,
     palette: ["rgb(214, 152, 70)", "rgb(37, 31, 17)", "rgb(114, 62, 17)"],
-    orientation: landscape
+    orientation: "landscape"
     // palette: { primaryColor: "", secondaryColor: "", tertiaryColor: "" }
     //rainyPalette: {}, nightPalette: {} etc
   },
@@ -25,227 +25,226 @@ let paintings = [
     paintingName: "Madonna and Child",
     objectID: 459136,
     palette: ["rgb(11, 6, 9)", "rgb(216, 152, 69)", "rgb(163, 5, 4)"],
-    orientation: portrait
+    orientation: "portrait"
 
   },
   {
     paintingName: "The Dance Class",
     objectID: 438817,
     palette: ["rgb(85, 94, 56)", "rgb(88, 58, 26)", "rgb(150, 140, 113)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Whalers",
     objectID: 437854,
     palette: ["rgb(219, 201, 169)", "rgb(193, 180, 147)", "rgb(39, 28, 15)"],
-    orientation: landscape
+    orientation: "landscape"
 
   },
   {
     paintingName: "View of Toledo",
     objectID: 436575,
     palette: ["rgb(35, 81, 89)", "rgb(71, 68, 49)", "rgb(90, 103, 62)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Young Woman with a Water Pitcher",
     objectID: 437881,
     palette: ["rgb(17, 40, 64)", "rgb(141, 133, 112)", "rgb(38, 20, 23)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Boating",
     objectID: 436947,
     palette: ["rgb(101, 134, 166)", "rgb(89, 57, 36)", "rgb(216, 212, 209)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "La Berceuse",
     objectID: 437984,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Pines Along the Shore",
     objectID: 459095,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Windflowers",
     objectID: 10767,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "The Calm Sea",
     objectID: 436005,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "The Veteran in a New Field",
     objectID: 11145,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Marine Landscape",
     objectID: 10215,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Steamboats in the Port of Rouen",
     objectID: 437309,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Allée of Chestnut Trees",
     objectID: 459121,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Sunflowers",
     objectID: 436524,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "View of the Domaine Saint-Joseph",
     objectID: 435885,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Oleanders",
     objectID: 436530,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Venice, from the Porch of Madonna della Salute",
     objectID: 437853,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Dancers, Pink and Green",
     objectID: 436140,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Breton Brother and Sister",
     objectID: 435754,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Olive Trees",
     objectID: 437998,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Roman Girl at a Fountain",
     objectID: 435708,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Kynance",
     objectID: 435775,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "The House with the Cracked Walls",
     objectID: 435874,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Dancers Practicing at the Barre",
     objectID: 436139,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Still Life with Apples and a Pot of Primroses",
     objectID: 435882,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Madame Cézanne (Hortense Fiquet, 1850–1922) in the Conservatory",
     objectID: 435875,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "The Road from Versailles to Louveciennes",
     objectID: 437685,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Sahurs Meadows in Morning Sun",
     objectID: 437683,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Washerwoman, Study",
     objectID: 437303,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "Irises",
     objectID: 436528,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Madame Manet (Suzanne Leenhoff, 1830–1906) at Bellevue",
     objectID: 438002,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "The Gulf of Marseilles Seen from L'Estaque",
     objectID: 435872,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Wheat Field with Cypresses",
     objectID: 436535,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "Roses",
     objectID: 436534,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: portrait
+    orientation: "portrait"
   },
   {
     paintingName: "A Farm in Brittany",
     objectID: 436448,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
+    orientation: "landscape"
   },
   {
     paintingName: "The Siesta",
     objectID: 436449,
     palette: ["rgb(29, 89, 68)", "rgb(142, 16, 16)", "rgb(191, 130, 24)"],
-    orientation: landscape
-  },
-
+    orientation: "landscape"
+  }
 ]
 
 let paintingTitle;
@@ -318,6 +317,7 @@ console.log(paintingNumber);
     $(".painting-name").text(paintingTitle);
     $(".painting-info").text(artistBio);
     $(".painting-info2").text(year + " " + medium);
+
 
 
 
--- assets/javascript/geo.js
+++ assets/javascript/geo.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
 
 
 
-
   $(".geoBox1").attr("style", "background-image: linear-gradient(to top, " + thisPainting.palette[0] + ", " + thisPainting.palette[1] + ");");
   $(".geoBox2").attr("style", "background-image: linear-gradient(to right, " + thisPainting.palette[1] + ", " + thisPainting.palette[2] + ");");
   $(".geoBox3").attr("style", "background-image: linear-gradient(to right, " + thisPainting.palette[0] + ", " + thisPainting.palette[2] + ");");
@@ -21,18 +20,3 @@ $(document).ready(function(){
 
 
 
-
-//   $(".geoBox1").attr("style", "background-color: white;");
-
-//   $(".primary-color-background").attr("style", "background: " + thisPainting.palette[0] + ";");
-//   .grad1 {
-//     background-image: linear-gradient(to top,#528360, #c2dd8f);
-// }
-
-// .grad2 {
-//     background-image: linear-gradient(to right, rgb(137, 164, 204), #1F4C8F);
-// }
-
-// .grad3 {
-//     background-image: linear-gradient(to right, #80a738, #1F4C8F);
-// }
